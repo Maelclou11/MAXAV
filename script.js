764e880b5fe9976ded99605f130e420071509285
Your Name <xavier.breton30@gmail.com>
2023-02-02 00:30:39 -0500

    Debut animation carte mieux Co-authored-by: Maelclou11 <Maelclou11@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*#region       FAQ  */
 const questions = document.querySelectorAll('.questions');
 
 questions.forEach((question) => {
@@ -13,3 +14,30 @@ buttonTarif.addEventListener('click', ()=> {
     beforeTarif.classList.add('clicked');
 
 });
+
+/* #endregion */
+/*#region       REVEAL ANIMATION  */
+// Style Général
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+  })
+
+// Reveal Accueil
+ScrollReveal().reveal('.contentAccueil h1', { delay: 500, origin: 'top'});
+ScrollReveal().reveal('.contentAccueil p', { delay: 800, origin: 'right', interval: 400});
+ScrollReveal().reveal('.boutonContainer', { delay: 800, origin: 'top'});
+ScrollReveal().reveal('.boutonContainer a', { delay: 800, origin: 'top', interval: 400});
+
+// Menu
+ScrollReveal().reveal('.ulNav li', { delay: 250, origin: 'left', interval: 200, distance: '30px'});
+/* #endregion */
+
+
+
+
+
+
+
