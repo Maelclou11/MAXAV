ae15c5e1dd322b1f67586a583310f380ded5684c
Your Name <xavier.breton30@gmail.com>
2023-02-03 12:47:37 -0500

    anime + texte
    Co-authored-by: Maelclou11 <Maelclou11@users.noreply.github.com>

--- script.js
+++ script.js
@@ -24,14 +24,39 @@ ScrollReveal({
   })
 
 // Reveal Accueil
-ScrollReveal().reveal('.contentAccueil h1', { delay: 500, origin: 'top'});
-ScrollReveal().reveal('.contentAccueil p', { delay: 800, origin: 'right', interval: 400});
-ScrollReveal().reveal('.boutonContainer', { delay: 800, origin: 'top'});
-ScrollReveal().reveal('.boutonContainer a', { delay: 800, origin: 'top', interval: 400});
+ScrollReveal().reveal('.contentAccueil h1', { delay: 200, origin: 'right'});
+ScrollReveal().reveal('.contentAccueil p', { delay: 400, origin: 'right', interval: 400});
+ScrollReveal().reveal('.boutonContainer', { delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('.boutonContainer a', { delay: 600, origin: 'bottom', interval: 400});
 
 // Menu
-ScrollReveal().reveal('.ulNav li', { delay: 250, origin: 'left', interval: 200, distance: '30px'});
-/* #endregion */
+ScrollReveal().reveal('.ulNav li', { delay: 250, origin: 'left', interval: 150, distance: '30px'});
+
+
+//Pourquoi nous?
+ScrollReveal().reveal('.sectionWhy .titleWhy', { delay: 800, origin: 'left'});
+ScrollReveal().reveal('.sectionWhy .contentWhy', { delay: 1000, origin: 'left', interval: 200});
+
+//points
+ScrollReveal().reveal('#points .container.haut .carte', { delay: 100, origin: 'right'});
+ScrollReveal().reveal('#points .container.haut img', { delay: 400, scale: 0.7, distance: '0px'});
+ScrollReveal().reveal('#points .container.haut .bandes', { delay: 400, origin: 'right', interval: 200});
+ScrollReveal().reveal('#points .container.bas .carte', { delay: 100, origin: 'left'});
+ScrollReveal().reveal('#points .container.bas img', { delay: 400, scale: 0.7, distance: '0px'});
+ScrollReveal().reveal('#points .container.bas .bandes', { delay: 400, origin: 'left', interval: 200});
+
+//Nos tarifs
+ScrollReveal().reveal('#tarifs .before__tarifs h3', { delay: 300, origin: 'top'});
+ScrollReveal().reveal('#tarifs .before__tarifs h3', { delay: 500, origin: 'top'});
+ScrollReveal().reveal('#tarifs a', { delay: 600, origin: 'top'});
+
+
+
+
+
+
+
+
 
 
 
